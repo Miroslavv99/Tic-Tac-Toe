db88492826e7bc823368989dc08fcbb2d9806d2a
Miroslav99 <mbochko99@gmail.com>
2024-12-14 16:27:13 +0200

    Add change name button logic

--- scripts.js
+++ scripts.js
@@ -1,20 +1,25 @@
 //Getting page elements
 const cells = document.querySelectorAll('.cell')
-const winerDisplay = document.querySelector('.winner')
+const winnerDisplay = document.querySelector('.winner')
 const resetButton = document.querySelector('.reset')
+const playerX = document.querySelector('#player-x')
+const playerO = document.querySelector('#player-o')
+const changeButton = document.querySelector('#change-player')
 
 //Factory function with closure containing methods for the game
 function gameController () {
     let gameBoard = [null, null, null, null, null, null, null, null, null] 
     let player = 'x'
     let gameOver = false
+    winnerDisplay.textContent = 'Let`s start the game!'
+    
 
-//Method handles clicks on game board cells, updates the game state, and switches players by calling the winner check function.
+    //Method handles clicks on game board cells, updates the game state, and switches players by calling the winner check function.
     const handleCheck = () => {
             cells.forEach(cell => {
                 const index = parseInt(cell.id)
                 cell.addEventListener('click', () => {
-                    if(gameBoard[index] === null && !gameOver) {
+                    if(gameBoard[index] === null && !gameOver && playerX.value !== '') {
                         gameBoard[index] = player
                         player = player === 'x' ? 'o' : 'x'
                         cell.textContent = player
@@ -24,7 +29,7 @@ function gameController () {
             })
         };
 
-//Method adding logic for checking the winner   
+    //Method adding logic for checking the winner   
     const  checkWinner =  () => {
             const winningCombinations = [
                 [0, 1, 2],
@@ -41,29 +46,43 @@ function gameController () {
                 const [a, b, c] = combination
 
                 if(gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[b] === gameBoard[c]) {
-                    winerDisplay.textContent = `Player ${player} Wins!`
+                        playerxValue = playerX.value
+                        playeroValue = playerO.value
+                   
+
+                    if(player === 'x') {
+                        winnerDisplay.textContent = `Player - ${playerxValue} Win!`
+                    } else {
+                        winnerDisplay.textContent = `Player - ${playeroValue} Win!`
+                    }
                     gameOver = true
+                    return
                 } 
             }
 
             if(!gameBoard.includes(null)) {
-                winerDisplay.textContent = 'Its Draw!'
+                winnerDisplay.textContent = 'Its Draw!'
                 gameOver = true
             }
         }
 
-        //Adds an event handler to the reset button that clears the game board, resets the game state, and allows a new game to be started
+    //Adds an event handler to the reset button that clears the game board, resets the game state, and allows a new game to be started
     resetButton.addEventListener('click', () => {
         gameBoard = [null, null, null, null, null, null, null, null, null]
         cells.forEach(cell => {
             cell.textContent = ''
         })
         player = 'x'
-        winerDisplay.textContent = 'Let`s Start The Game!'
+        winnerDisplay.textContent = 'Let`s Start The Game!'
         gameOver = false
     })
 
-     handleCheck()
+    changeButton.addEventListener('click', () => {
+        playerX.value = ''
+        playerO.value = ''
+    })
+
+    handleCheck()
     
     }
 
